db9a5d1a5abde8b93a8feb6520f4853bcddb14f0
Hau Pham <qhauict13@gmail.com>
2016-04-23 09:23:26 +0700

    123123123

--- main.c
+++ main.c
@@ -6,50 +6,7 @@
      if(check_file_exist("dict_db"))
           tree_word = btopn("dict_db", 0, FALSE);
      else{
-          tree_word = btcrt("dict_db", 0, FALSE);
-          load_sample_2_tree(tree_word, "FOLDOC");
-     }
 
-     if(check_file_exist("soundex_db"))
-          tree_soundex = btopn("soundex_db", 0, FALSE);
-     else{
-          tree_soundex = btcrt("soundex_db", 0, FALSE);
-          load_soundex_db(tree_soundex, tree_word);
-     }
-     
-     /* Init GTK+ */
-     gtk_init(&argc, &argv);
-
-     /* Create new GtkBuilder object */
-     builder = gtk_builder_new();
-     if (!gtk_builder_add_from_file(builder, UI_FILE, &error))
-     {
-          g_warning("%s", error->message);
-          g_free(error);
-          return 1;
-     }
-
-     /* Allocate data structure */
-     data = g_slice_new(ChData);
-
-     /* Get objects from UI */
-#define GW(name) CH_GET_WIDGET(builder, name, data)
-     GW(main_window);
-     GW(dlg_add);
-     GW(dlg_help);
-     GW(dlg_delete);
-     GW(dlg_edit);
-
-
-     GW(entry_add);
-     GW(txt_meaning_add);
-     GW(treeview_result_main);
-     GW(entry_search_main);
-     GW(txt_meaning_main);
-     GW(word_label_edit);
-     GW(txt_meaning_edit);
-     GW(list_store1);
-     GW(list_store2);
      data->tree_word = tree_word;
      data->tree_soundex = tree_soundex;
      data->stack = stack;
